51f02e864774ca3809a46de69ff50d9e8651ba0d
GabrielMosconegi <gabriel.gallina@taking.com.br>
2024-07-08 16:52:05 -0300

    fix: qr code button just on game

--- src/components/Modal/ModalQrCodeGame.tsx
+++ src/components/Modal/ModalQrCodeGame.tsx
@@ -1,35 +1,33 @@
 import React from 'react'
 import { Modal } from 'react-bootstrap'
-import QRCode from "react-qr-code";
-import { useLocation } from 'react-router-dom';
+import QRCode from 'react-qr-code'
 
 interface ModalProps {
   show: boolean
   setShow: (value: boolean) => void
 }
 
-function ModalQrCodeGame({
-  show,
-  setShow,
-}: ModalProps) {
-    const location = window.location.href;
+function ModalQrCodeGame({ show, setShow }: ModalProps) {
+  const location = window.location.href
 
-    const handleClose = () => {
-        setShow(false)
-    }
+  const handleClose = () => {
+    setShow(false)
+  }
 
-    return (
-      <Modal show={show} centered onHide={handleClose}>
-        <Modal.Header closeButton className='custom-modal-header'>
-          <Modal.Title className='text-center w-100'>Convide jogadores!</Modal.Title>
-        </Modal.Header>
-        <Modal.Dialog className='custom-modal-dialog'>
-          <Modal.Body className='d-flex justify-content-center'>
-            <QRCode value={location} />
-          </Modal.Body>
-        </Modal.Dialog>
-      </Modal>
-    )   
+  return (
+    <Modal show={show} centered onHide={handleClose}>
+      <Modal.Header closeButton className="custom-modal-header">
+        <Modal.Title className="text-center w-100">
+          Convide jogadores!
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Dialog className="custom-modal-dialog">
+        <Modal.Body className="d-flex justify-content-center">
+          <QRCode value={location} />
+        </Modal.Body>
+      </Modal.Dialog>
+    </Modal>
+  )
 }
 
 export default ModalQrCodeGame
